4d9ec68c61bbc2837dfd5ea9196860f6e5b75581
SaulAlamilla <l18390023@chetumal.tecnm.mx>
2021-12-11 00:03:58 -0500

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,27 @@
+/*
+* Alumno: Saul Humberto Alamilla Calixto
+* No. Control 18390023
+* */
+function loadCountryInfo(){
+    const url = 'https://restcountries.com/v3.1/name/italy';
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+    document.getElementById("info").innerHTML = `Nombre común: ${data[0].name.common}
+													<br>Nombre oficial: ${data[0].name.official}
+													<br>Modena: ${data[0].currencies.EUR.name}
+													<br>Simbolo de la modena: ${data[0].currencies.EUR.symbol}`;
+    })
+}
+function loadCountryFlag(){
+    const url = 'https://flagcdn.com/256x192/it.png';
+    fetch(url)
+    .then(response => response.blob())
+    .then(imageBlob => {
+    const imageObjectURL = URL.createObjectURL(imageBlob);
+    const image = new Image();
+    image.src = imageObjectURL
+
+    document.getElementById("flag").appendChild(image);
+    })
+}
